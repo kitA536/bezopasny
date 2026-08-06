8def7c6cb37973d5b1cc8cb7073874a4e13760c0
nikita <sinitsyn1204@gmail.com>
2026-08-06 19:50:21 +0300

    Update create_tables.sql

--- big-data-bd/create_tables.sql
+++ big-data-bd/create_tables.sql
@@ -4,3 +4,6 @@ CREATE TABLE users (
     name VARCHAR(50),
     age INT
 ); - создать таблицу для пользователей
+SELECT * FROM users; - выбрать все
+SELECT name FROM users; - выбрать только имена
+SELECT * FROM users WHERE age > 18; - выбрать только взрослых
