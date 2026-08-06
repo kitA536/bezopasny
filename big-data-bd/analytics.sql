2829be7a1d83a6db9e2e731b20c1166f0e9e9efd
nikita <sinitsyn1204@gmail.com>
2026-08-06 19:55:27 +0300

    Update analytics.sql

--- big-data-bd/analytics.sql
+++ big-data-bd/analytics.sql
@@ -1,1 +1,6 @@
 SELECT AVG(age) ...
+SELECT COUNT(*) FROM users; - посчитать кол-во пользоваелей
+FROM users ORDER BY age DESC; - сортировка по убыванию
+FROM users ORDER BY age ASC; - сортировка по возрастанию
+SELECT AVG(age) FROM users; - срений возраст
+SELECT MAX or MIN(age) FROM users; - самый младший / самый старший
